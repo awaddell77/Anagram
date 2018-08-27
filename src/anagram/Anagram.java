dfb80e50f3dc439f2b281ae9b6a498fb2aabd575
awaddell77 <waddell.andrew@gmail.com>
2018-08-27 11:00:37 -0700

    Updates

--- src/anagram/Anagram.java
+++ src/anagram/Anagram.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package anagram;
 
 import java.util.HashMap;
 
-/**
- *
- * @author awaddell
- */
 public class Anagram {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         String testStr = "cinema";
@@ -32,16 +21,16 @@ public class Anagram {
         }
     }
     public static HashMap<Character, Integer> splitChars(String str){
+        //hashmap is used in order to catch duplicates in an inexpensive manner
+        //while also allowing us to track how many duplicates there are per character
         HashMap<Character, Integer> tempHash = new HashMap<>();
         for (int i = 0; i < str.length(); i++){
             if (tempHash.containsKey(str.charAt(i))){
                 tempHash.put(str.charAt(i), tempHash.get(str.charAt(i)) + 1);
             }
             else{
-                tempHash.put(str.charAt(i), 1);
-                
+                tempHash.put(str.charAt(i), 1);  
             }
-            
         }
         return tempHash;
     }
