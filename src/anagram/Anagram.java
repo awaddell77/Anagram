8bbd39f691629d7db520764a7ec4e39fb19c2297
awaddell77 <waddell.andrew@gmail.com>
2018-08-28 11:13:07 -0700

    Updates

--- src/anagram/Anagram.java
+++ src/anagram/Anagram.java
@@ -7,10 +7,12 @@ public class Anagram {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        String testStr = "cinema";
-        String testStr2 = "iceman";
+        //String testStr = "cinema";
+        //String testStr2 = "iceman";
+        String testStr = args[0];
+        String testStr2 = args[1];
         HashMap res = splitChars(testStr2);
-        System.out.println(res.toString());
+        //System.out.println(res.toString());
         boolean result = checkAnagram(testStr, testStr2);
         if (result){
             System.out.println(testStr + " is an anagram for " + testStr2);
@@ -20,7 +22,7 @@ public class Anagram {
             System.out.println(testStr + " is not an anagram for " + testStr2);
         }
     }
-    public static HashMap<Character, Integer> splitChars(String str){
+    private static HashMap<Character, Integer> splitChars(String str){
         //hashmap is used in order to catch duplicates in an inexpensive manner
         //while also allowing us to track how many duplicates there are per character
         HashMap<Character, Integer> tempHash = new HashMap<>();
@@ -28,13 +30,14 @@ public class Anagram {
             if (tempHash.containsKey(str.charAt(i))){
                 tempHash.put(str.charAt(i), tempHash.get(str.charAt(i)) + 1);
             }
-            else{
+
+            else if (str.charAt(i) != ' '){
                 tempHash.put(str.charAt(i), 1);  
             }
         }
         return tempHash;
     }
-    public static boolean checkAnagram(String str, String str2){
+    private static boolean checkAnagram(String str, String str2){
         HashMap strHash = splitChars(str);
         HashMap str2Hash = splitChars(str2);
         return strHash.equals(str2Hash);
